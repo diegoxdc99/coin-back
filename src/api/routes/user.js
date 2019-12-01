7b69d79146ed98d1e927fea49b0fd0597876ac03
Diego Alberto Meneses Tamayo <diego.meneses@sophos.col.com>
2019-11-30 20:22:08 -0500

    basic auth and helmet

--- src/api/routes/user.js
+++ src/api/routes/user.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const passport = require('passport')
 const validator = require('express-joi-validation').createValidator({})
 const { userMiddleware } = require('../middleware')
 const { users } = require('../../controller')
@@ -10,4 +11,5 @@ module.exports = (app) => {
   app.use('/users', route) // Creating a user route
 
   route.post('/', validator.body(userSchema), userMiddleware.existUser, users.createUser)
+  route.post('/login', passport.authenticate('basic', { session: false }))
 }
--- src/loaders/express.js
+++ src/loaders/express.js
@@ -1,13 +1,17 @@
 const bodyParser = require('body-parser')
+const passport = require('passport')
+const helmet = require('helmet')
 const routes = require('../api')
 const { handleError } = require('../helpers/error')
 
 module.exports = async (app) => {
+  app.use(helmet())
+  app.use(bodyParser.json())
+  app.use(passport.initialize())
+
   app.get('/status', (req, res) => { // Path to know the status of the server
     res.status(200).end()
   })
-
-  app.use(bodyParser.json()) // Middleware to parse the body into a json
   app.use('/', routes())
   app.use((err, req, res, next) => {
     handleError(err, res)
