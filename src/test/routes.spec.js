509ba6819ec998fedd01adef69002637d4cc824a
diegoxdc99 <diegoxdc99@hotmail.com>
2019-12-14 20:32:35 -0500

    update test

--- src/test/routes.spec.js
+++ src/test/routes.spec.js
@@ -3,11 +3,26 @@ const app = require('../app')
 const cryptoService = require('../services/cryptocurrency')
 
 const token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImNhcmxvcyIsImlkIjoyLCJpYXQiOjE1NzYzNDkwNTAsImV4cCI6MTU3NjQzNTQ1MH0.56REMJUklQeZJ3ri1y5mISElsj9sx10phtqb_JfAArw'
+const tokenNotFound = {
+  status: 'error',
+  statusCode: 401,
+  message: 'No authorization token was found'
+}
 let server
 let agent
 
+jest.mock('../repository/cryptocurrency', () => {
+  return () => {
+    return {
+      findAllByUser: jest.fn().mockResolvedValueOnce([{ name: 'ANC' }])
+    }
+  }
+})
+
+// jest.unmock('../repository/cryptocurrency')
+
 describe('Get coins', () => {
-  beforeEach((done) => {
+  beforeAll((done) => {
     server = app.listen(4000, (err) => {
       if (err) return done(err)
       agent = supertest.agent(server)
@@ -15,15 +30,14 @@ describe('Get coins', () => {
     })
   })
 
-  afterEach((done) => {
+  afterAll((done) => {
     return server && server.close(done)
   })
   it('Get 200', async () => {
-    jest.spyOn(cryptoService, 'findAllByUser').mockResolvedValue([{ name: 'AC' }])
     const response = await agent.get('/cryptos/me').set('Authorization', token).send()
+    console.log('response.body 1 :', response.body)
     expect(response.body).toBeDefined()
     expect(response.status).toBe(200)
-    expect(cryptoService.findAllByUser).toBeCalled()
   })
 
   it('Get 500', async () => {
@@ -32,5 +46,13 @@ describe('Get coins', () => {
     expect(response.body).toBeDefined()
     expect(response.status).toBe(500)
     expect(cryptoService.findAllByUser).toBeCalled()
+  })
+
+  it('Get 401 without token authorization', async () => {
+    const response = await agent.get('/cryptos/me').send()
+    console.log('response.body :', response.body)
+    expect(response.body).toBeDefined()
+    expect(response.body).toStrictEqual(tokenNotFound)
+    expect(response.status).toBe(401)
   })
 })
